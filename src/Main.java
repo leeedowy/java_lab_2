dca6e9bb045058ac4eb5994b3c66eefcffe1c50a
kmuller86 <krzysztof.muller@wp.pl>
2022-01-17 18:09:05 +0100

    ćw 7

--- src/Main.java
+++ src/Main.java
@@ -6,59 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* Przyklad_7* Mapy - Tylko dla chętnych */
+        /* Cw.7* Mapy - Tylko dla chętnych */
 
-        /* mapy są strukturami które przyjmują klucz i wartość */
-        Map<Integer, String> shapes = new HashMap<Integer, String>();
-        shapes.put(1, "Kwadrat"); // dodawanie pary klucz - wartość
-        shapes.put(2, "Koło");
-        shapes.put(4, "Trójkąt");
-        shapes.put(8, "Ośmiobok");
+        /* Zad.1* Napisać program:
+            a) Stworzenie mapę: Map<String, String>,
+            b) uzupełnić wartościami (dowolność), przynajmniej 5 rekordów,
+            c) usunąć 5-tą parę, dodać 2-ie dodatkowe pary i podwienić wartość w 1-ej i 2-giej parze (wartości dowolne),
+            d) wyświetlić zawartość mapy i wyświetlić za pomocą pętli
+        */
 
-        System.out.println("Kształty: " + shapes);
-        System.out.println("Wyświetl wartość dla klucza nr2: " + shapes.get(2)); // get - pobranie wartości dla klucza
-
-        /* Podmienianie wartości dla danego klucza */
-        shapes.replace(2, "Trapez");
-        System.out.println("Wyświetl wartość dla klucza nr2: " + shapes.get(2) + "\n");
-        wyswietlMape(shapes);
-
-        /* usówanie wartości */
-        shapes.remove(4);
-        shapes.put(7, "Siedmiobok");
-        System.out.println();
-        wyswietlMape(shapes);
-
-        /* kluczem i wartością mogą być dowolne typy Objektowe np. <Character, Integer>, <Object, Object>, <String, String>, <Long, Set<String>> */
-        Map<Character, Integer> numbers = new HashMap<Character, Integer>();
-        numbers.put('Z', 2); // dodawanie pary klucz - wartość
-        numbers.put('Y', 76);
-        numbers.put('C', 28);
-        numbers.put('A', 6);
-        System.out.println();
-        wyswietlMape(numbers);
-//        wyswietlMape2(numbers); // jeśli nie chcemy używać metody generycznej
-    }
-
-    /* Map<?, ?> map - znaki zapytania informują nas że jest to metoda generyczna (taka która może przyjąć dowolne typy),
-     * można oczywiście wytypować tą metodę według potrzeby np. wyswietlMape(Map<Integer, String> map) */
-//    public static void wyswietlMape(Map<Integer, String> map) {  // jeśli nie chcemy używać metody generycznej
-    public static void wyswietlMape(Map<?, ?> map) {
-        for (Map.Entry<?, ?> entry : map.entrySet()) {
-            System.out.print("entry: " + entry + ", "); // klucz - wartość
-            System.out.print(" Key: " + entry.getKey() + ", "); // klucz
-            System.out.print(" Value: " + entry.getValue() + "; \n"); // wartość
-        }
-        System.out.print("\nTylko wartości: " + map.values() + ", "); // Wszystkie wartości
-    }
-
-//    public static void wyswietlMape2(Map<Character, Integer> map) {
-//        for (Map.Entry<?, ?> entry : map.entrySet()) {
-//            System.out.print("entry: " + entry + ", "); // klucz - wartość
-//            System.out.print(" Key: " + entry.getKey() + ", "); // klucz
-//            System.out.print(" Value: " + entry.getValue() + "; \n"); // wartość
-//        }
-//        System.out.print("\nTylko wartości: " + map.values() + ", "); // Wszystkie wartości
-//    }
+        /* zad.2* Napisać program:
+            a) Stworzyć mapę Map<Long, String>,
+            b) wstawić 5 par klucz-wartość z klawiatury (Wprowadzamy za pomocą dowolnej pętli),
+            c) wykonać dowolne operacje dodawania, usuwania, zamiany wartości,
+            d) znaleść parę o wartości z najdłuższą długościa wyrazu (map.getValue().length - jak największy),
+            e) przeiterować i wyświetlić wartości w pętli
+        */
 
 }
