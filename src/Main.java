2b426827abb9dcb49078cd6f8afa57dfcd6977d9
Patryk Zieliński <patryk.zielinski.stg@gmail.com>
2022-01-22 02:19:56 +0100

    Zadanie 7

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,23 @@ public class Main {
             d) wyświetlić zawartość mapy i wyświetlić za pomocą pętli
         */
 
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            map.put(scanner.next(), scanner.next());
+        }
+
+        map.remove(map.keySet().toArray(new String[0])[map.keySet().toArray(new String[0]).length-1]);
+        for (int i = 0; i < 2; i++) {
+            map.put(map.keySet().toArray(new String[0])[0], scanner.next());
+            map.put(map.keySet().toArray(new String[0])[1], scanner.next());
+        }
+
+        System.out.println(map);
+        for (String key : map.keySet()) {
+            System.out.printf("%s=%s%n", key, map.get(key));
+        }
+
         /* zad.2* Napisać program:
             a) Stworzyć mapę Map<Long, String>,
             b) wstawić 5 par klucz-wartość z klawiatury (Klucz wprowadzamy jako indeks za pomocą pętli for),
@@ -23,4 +40,21 @@ public class Main {
             e) przeiterować i wyświetlić wartości w pętli
         */
 
+        Map<Integer, String> map2 = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            map2.put(i, scanner.next());
+        }
+
+        map2.remove(3);
+        map2.put(10, scanner.next());
+        map2.put(0, scanner.next());
+
+        Map.Entry<Integer, String> max = map2.entrySet().iterator().next();
+        for (Map.Entry<Integer, String> entry : map2.entrySet()) {
+            if (entry.getValue().length() > max.getValue().length()) {
+                max = entry;
+            }
+        }
+        System.out.println(max);
+    }
 }
